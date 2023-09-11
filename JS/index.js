d25bed5d01172705c7057dc1b1298385469a5415
calamadrid18 <142576091+calamadrid18@users.noreply.github.com>
2023-09-11 14:11:19 -0500

    cambios

--- JS/index.js
+++ JS/index.js
@@ -1,38 +1,76 @@
-const menu = document.querySelector(".buttonJS")
-const menuList = document.querySelector(".perro")
-console.log(menu);
-console.log(menuList);
+const menu = document.querySelector(".buttonJS");
+const menuList = document.querySelector(".perro");
 
-menu.addEventListener('click', function () {
-    menuList.classList.toggle('d-none')
-})
-
-
+menu.addEventListener("click", function () {
+  menuList.classList.toggle("d-none");
+});
 
 const misTarjetas = document.querySelector(".misTarjetas");
-for (let i = 0; i < data.events.length; i++) {
-    
-    let nuevaEtiqueta = document.createElement("div");
-    nuevaEtiqueta.innerHTML = `<div class="col-xl-4  col-md-4 col-sm-12  pb-3 pt-3">
-<div class="card" style="width: 18rem;">
-    <img src="${data.events[i].image}" class="card-img-top custom-card-img" alt="food">
-    <div class="card-body">
-        <h5 class="card-title">${data.events[i].name}</h5>
-        <div style="height: 80px;">
-        <p class="card-text">${data.events[i].description}</p>
-        </div>
-        <div class="d-flex justify-content-between">
-        <h5>Price: ${data.events[i].price}</h5>
-        <a href="./detail.html">DetailS</a>
-        </div>
-    </div>
-</div>
-</div>`
-misTarjetas.appendChild(nuevaEtiqueta)
+const categoryCheckboxes = document.querySelectorAll('input[name="category"]');
+
+let selectedCategory = "todos";
+
+function handleCheckboxChange(event) {
+  misTarjetas.innerHTML = "";
+  const newCategory = event.target.value;
+
+  categoryCheckboxes.forEach((checkbox) => {
+    if (checkbox !== event.target) {
+      checkbox.checked = false;
+    }
+  });
+
+  selectedCategory = newCategory;
+
+  for (let i = 0; i < data.events.length; i++) {
+    const event = data.events[i];
+
+    if (isChecked("todos") || isChecked(event.category)) {
+      let nuevaEtiqueta = document.createElement("div");
+      nuevaEtiqueta.innerHTML = `<div class="col-lg-3 col-md-4 col-sm-12 pb-3 pt-3">
+            <div class="card" style="width: 18rem;">
+            <img src="${event.image}" class="card-img-top custom-card-img" alt="food">
+            <div class="card-body">
+                <h5 class="card-title">${event.name}</h5>
+                <div style="height: 80px;">
+                <p class="card-text">${event.description}</p>
+                </div>
+                <div class="d-flex justify-content-between">
+                <h5>Price: ${event.price}</h5>
+                <a href="./detail.html">Details</a>
+                </div>
+            </div>
+            </div>
+        </div>`;
+      misTarjetas.appendChild(nuevaEtiqueta);
+    }
+  }
 }
 
+for (const checkbox of categoryCheckboxes) {
+  checkbox.addEventListener("change", handleCheckboxChange);
+}
 
+function isChecked(value) {
+  const checkbox = document.querySelector(
+    `input[name="category"][value="${value}"]`
+  );
+  return checkbox && checkbox.checked;
+}
 
+handleCheckboxChange({
+  target: document.querySelector('input[name="category"][value="todos"]'),
+});
 
+const formularioBuscador = document.getElementById("formularioBuscador");
+const buscadorInput = document.getElementById("buscadorInput");
 
+function mostrarAlerta(buscador) {
+  alert(`${buscador}`);
+}
 
+formularioBuscador.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const buscador = buscadorInput.value.toLowerCase();
+  mostrarAlerta(buscador);
+});
