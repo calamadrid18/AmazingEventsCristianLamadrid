f0d03513fb5eacbc024867c869486ac26466f115
calamadrid18 <142576091+calamadrid18@users.noreply.github.com>
2023-09-19 15:44:20 -0500

    js

--- JS/upcomingevents.js
+++ JS/upcomingevents.js
@@ -1,19 +1,19 @@
-import { imprimirCheckBox, imprimirSelect, imprimirCards,  filtrosCruzados, filtroFechaUpcoming, search} from "../modules/function.js";
+import { imprimirCheckBox, imprimirSelect, imprimirCards, filtrosCruzados, filtroFechaUpcoming, search } from "../modules/function.js";
 
 const URL_API = 'https://mindhub-xj03.onrender.com/api/amazing';
 
 const contenedorChecks = document.getElementById("contenedorChecks");
 const select = document.getElementById("selects");
 const contenedoresCards = document.getElementById("contenedoresCards");
-const searchDiv = (document.querySelector(".name"));
+const searchDiv = document.querySelector(".name");
 
 fetch(URL_API)
   .then(response => response.json())
   .then(data => {
     const events = data.events;
-    const currentDate= data.currentDate;
+    const currentDate = data.currentDate;
     const arrayNombreEvents = events.map(objetoEvents => objetoEvents.name);
-    let upComingEvents=filtroFechaUpcoming(events,currentDate)
+    let upComingEvents = filtroFechaUpcoming(events, currentDate);
     console.log(upComingEvents);
     imprimirCards(upComingEvents, contenedoresCards);
 
@@ -21,8 +21,9 @@ fetch(URL_API)
     imprimirCheckBox(contenedorChecks, Array.from(listaCategorias));
 
     contenedorChecks.addEventListener("change", () => {
-      const returnCruzados = filtrosCruzados(upComingEvents, null);
-      imprimirCards(returnCruzados, contenedoresCards);
+      const selectedCategories = obtenerCategoriasSeleccionadas();
+      const eventosFiltrados = filtradoPorCheckUpcoming(upComingEvents, selectedCategories);
+      imprimirCards(eventosFiltrados, contenedoresCards);
     });
 
     select.addEventListener("change", () => {
@@ -33,9 +34,26 @@ fetch(URL_API)
   })
   .catch(err => console.log(err));
 
-  searchDiv.addEventListener("keyup",()=>{
-    let impEvents= document.querySelectorAll(".tarjeta")
-    console.log(impEvents);
-    let searchValue = searchDiv.value;
-    search(searchValue,impEvents)
-  })
+searchDiv.addEventListener("keyup", () => {
+  let impEvents = document.querySelectorAll(".tarjeta");
+  let searchValue = searchDiv.value;
+  search(searchValue, impEvents);
+});
+
+// Función para obtener las categorías seleccionadas en las casillas de verificación
+function obtenerCategoriasSeleccionadas() {
+  const checkboxes = document.querySelectorAll("input[type=checkbox]:checked");
+  const selectedCategories = Array.from(checkboxes).map(checkbox => checkbox.value);
+  return selectedCategories;
+}
+
+// Función para filtrar eventos futuros en función de las categorías seleccionadas
+function filtradoPorCheckUpcoming(upComingEvents, selectedCategories) {
+  if (!selectedCategories || selectedCategories.length === 0) {
+    // Si no se seleccionan categorías, devolver todos los eventos futuros
+    return upComingEvents;
+  } else {
+    // Filtrar eventos futuros en función de las categorías seleccionadas
+    return upComingEvents.filter(evento => selectedCategories.includes(evento.category));
+  }
+}
